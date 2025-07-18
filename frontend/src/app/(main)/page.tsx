2a0dd34e739c7323c9c38f30fc6264b6d7641baa
jaiden-lee <jaidenwlee@gmail.com>
2025-07-17 20:59:21 -0400

    redirect to integrations if not integrated with jira

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,4 +1,25 @@
-export default function Home() {
+import { junctureBackend } from "@/lib/juncture/backend";
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+
+  const { orgId } = await auth();
+
+  // will never happen
+  if (!orgId) {
+    return null
+  }
+
+  const status = await junctureBackend.general.checkConnectionValidity({
+    externalId: orgId,
+    provider: 'jira'
+  });
+
+  if (!status.exists || status.isInvalid) {
+    return redirect("/integrations");
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
